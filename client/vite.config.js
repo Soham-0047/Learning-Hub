40fc3fbad55efca2f39a9493f2199a1ee6f02dcf
Soham <solutionnoton@gmail.com>
2023-11-17 21:13:12 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+//http://localhost:5000
 export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:5000',
+        target:'https://learning-hub.onrender.com',
         secure:false,
       },
     },
